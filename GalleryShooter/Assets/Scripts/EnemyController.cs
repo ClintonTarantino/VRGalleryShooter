5297889d686df4a1e8b5b30877d0319d720eda4a
Clint S Magzul <cmagzul92@gmail.com>
2017-12-07 00:29:14 -0500

    v1
    Added Background
    Enemy Sprites
    Looping BGM
    
    Worked on Enemies
    Enemies Spawning at random range (1-3 seconds)
    Enemy dies when you press the trigger on em'

---
+++ GalleryShooter/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Collider))]
+public class EnemyController : MonoBehaviour {
+
+    CardboardHead head;
+    Vector3 startPosition;
+    SpriteRenderer enemyRenderer;
+    Collider enemyCollider;
+    AudioSource deadSound;
+
+    void Start ()
+    {
+        head = Camera.main.GetComponent<StereoController>().Head;
+        startPosition = transform.localPosition;
+
+        deadSound = GetComponent<AudioSource>();
+        enemyRenderer = GetComponent<SpriteRenderer>();
+        enemyCollider = GetComponent<Collider>();
+
+        enemyRenderer.enabled = false;
+        enemyCollider.enabled = false;
+
+
+        StartCoroutine(SpawnRate());
+    }
+
+    void Update ()
+    {
+        RaycastHit hit;
+        bool isLookedAt = GetComponent<Collider>().Raycast(head.Gaze, out hit, Mathf.Infinity);
+        if (Cardboard.SDK.Triggered && isLookedAt)
+        
+            EnemyDead();
+        
+    }
+
+    IEnumerator SpawnRate()
+    {
+        yield return new WaitForSeconds(Random.Range(1f, 3f));
+        enemyCollider.enabled = true;
+        enemyRenderer.enabled = true;
+        yield return new WaitForSeconds(Random.Range(1f, 3f));
+        enemyCollider.enabled = false;
+        enemyRenderer.enabled = false;
+        StartCoroutine(SpawnRate());
+    }
+
+    public void EnemyDead()
+    {
+        enemyCollider.enabled = false;
+        enemyRenderer.enabled = false;
+
+        deadSound.Play();
+    }
+
+}
